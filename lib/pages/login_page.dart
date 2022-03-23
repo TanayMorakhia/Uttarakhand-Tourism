496a6f2089aba4f74a9c8f722ff1aac39cbe8745
Arpit Tripathi <tripathiarpit793@gmail.com>
2022-03-23 13:08:10 +0530

    added login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import 'package:uttarakhand_tourism/utils/routes.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 100.0,
+              ),
+              Image.asset(
+                "assets/images/logo.png",
+                fit: BoxFit.cover,
+              ),
+              Text(
+                "Welcome",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 32.0),
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      child: Text("Sign in with Google"),
+                      style: TextButton.styleFrom(minimumSize: Size(180, 42)),
+                    ),
+                    // TextFormField(
+                    //   decoration: InputDecoration(
+                    //     hintText: "Enter username",
+                    //     labelText: "Username",
+                    //   ),
+                    //   validator: (value) {
+                    //     if (value!.isEmpty) {
+                    //       return "Username cannot be empty";
+                    //     }
+                    //     return null;
+                    //   },
+                    // ),
+                    // TextFormField(
+                    //   obscureText: true,
+                    //   decoration: InputDecoration(
+                    //     hintText: "Enter Password",
+                    //     labelText: "Password",
+                    //   ),
+                    //   validator: (value) {
+                    //     if (value!.isEmpty) {
+                    //       return "Password cannot be empty";
+                    //     }
+                    //     return null;
+                    //   },
+                    // ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    // ElevatedButton(
+                    //   onPressed: () {
+                    //     // Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //   },
+                    //   child: Text("Login"),
+                    //   style: TextButton.styleFrom(minimumSize: Size(120, 40)),
+                    // )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
